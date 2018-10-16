c8d453a16a643a8be4e26bb056a61b7fef28b6d6
japloon <jeejl-wa14@student.tarc.edu.my>
2018-10-16 15:49:54 +0800

    update

--- app/src/main/java/com/example/taruc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.taruc.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
